b0f729d9e169e155e7ee8c56793cac33bd71ddfe
Alexander Kvenvolden <kvenclassical@gmail.com>
2018-05-21 09:34:51 -0700

    Got a fakeit mock with Google test framework.

---
+++ kwh-modbus-tests/test.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+#include "fakeit.hpp"
+
+using namespace fakeit;
+
+class Test1
+{
+public:
+	virtual int Sum(int x, int y) = 0;
+};
+
+class Test2 : public Test1
+{
+public:
+	virtual int Sum(int x, int y)
+	{
+		return x + y;
+	}
+};
+
+TEST(TestCaseName, TestName) {
+	Mock<Test1> mock = Mock<Test1>();
+	When(Method(mock, Sum)).Return(6);
+	Test1 &ts = mock.get();
+	EXPECT_EQ(ts.Sum(1, 1), 5);
+  
+}
