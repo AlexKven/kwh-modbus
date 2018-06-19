bf3a40824934d83206df02063e77fe9cec60f5e1
Alexander Kvenvolden <kvenclassical@gmail.com>
2018-06-19 07:39:18 -0700

    Refactored the mock setup in ModbusSerialTests into the USE_MOCK macro.

--- kwh-modbus-tests/modbusserialtests.cpp
+++ kwh-modbus-tests/modbusserialtests.cpp
@@ -8,32 +8,49 @@
 #include "../kwh-modbus/noArduino/arduinofunctions.h"
 #include "test_helpers.h"
 
+#define USE_MOCK Mock<ISerialStream> mockSerial; \
+Fake(Method(mockSerial, begin)); \
+modbus->config(&mockSerial.get(), 1200, 4);
+
 using namespace fakeit;
 
 class ModbusSerialTests : public ::testing::Test
 {
 protected:
 	ModbusSerial<ISerialStream, ArduinoFunctions, ModbusMemory<Modbus>> *modbus = new ModbusSerial<ISerialStream, ArduinoFunctions, ModbusMemory<Modbus>>();
+
+	inline void setupMock()
+	{
+		Mock<ISerialStream> mockSerial;
+		Fake(Method(mockSerial, begin));
+		modbus->config(&mockSerial.get(), 1200, 4);
+	}
 };
 
 TEST_F(ModbusSerialTests, ModbusSerial_Config)
 {
-	Mock<ISerialStream> mockSerial;
-	Fake(Method(mockSerial, begin));
-	modbus->config(&mockSerial.get(), 1200, 4);
+	USE_MOCK
 
 	Verify(Method(mockSerial, begin).Using(1200)).Once();
 }
 
 TEST_F(ModbusSerialTests, ModbusSerial_Available)
 {
-	Mock<ISerialStream> mockSerial;
-	Fake(Method(mockSerial, begin));
+	USE_MOCK
 	When(Method(mockSerial, available)).Return(32);
 
-	modbus->config(&mockSerial.get(), 1200, 4);
 	int result = modbus->available();
 
 	ASSERT_EQ(result, 32);
 	Verify(Method(mockSerial, available)).Once();
+}
+
+TEST_F(ModbusSerialTests, ModbusSerial_Flush)
+{
+	USE_MOCK
+	Fake(Method(mockSerial, flush));
+
+	modbus->flush();
+
+	Verify(Method(mockSerial, flush)).Once();
 }
