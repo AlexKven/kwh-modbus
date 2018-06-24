12857f6409d3b5561d759e4b5bd37482961acf0d
Alexander Kvenvolden <kvenclassical@gmail.com>
2018-06-24 13:59:10 -0700

    Linux programs compile.

--- kwh-modbus/linuxCompile.sh
+++ kwh-modbus/linuxCompile.sh
@@ -1,3 +1,3 @@
 g++ mock/mockserialstream.cpp -D "NO_ARDUINO" -c
 g++ linuxPrograms/modbusmaster.cpp libraries/modbusMaster/modbusmaster.h -D "NO_ARDUINO" -o linuxOutputs/modbusMaster.out
-g++ linuxPrograms/modbusslave.cpp mock/mockserialstream.cpp noArduino/systemfunctions.cpp -D "NO_ARDUINO" -fpermissive -o linuxOutputs/modbusSlave.out
+g++ linuxPrograms/modbusslave.cpp libraries/modbus/modbus.cpp noArduino/modbusmemory.cpp mock/mockserialstream.cpp noArduino/systemfunctions.cpp -D "NO_ARDUINO" -fpermissive -o linuxOutputs/modbusSlave.out
--- kwh-modbus/linuxPrograms/modbusslave.cpp
+++ kwh-modbus/linuxPrograms/modbusslave.cpp
@@ -1,19 +1,13 @@
-//#include <sys/types.h>
-//#include <sys/stat.h>
-//#include <fcntl.h>
-//#include <cstdint>
-//#include <cstddef>
-//#include "../noArduino/arduinomacros.h"
-//#include "../libraries/modbus/modbusserial.h"
+#include "../libraries/modbus/modbusserial.h"
 #include "../mock/mockserialstream.h"
 #include "../noArduino/systemfunctions.h"
-//#include "../noArduino/modbusmemory.h"
+#include "../noArduino/modbusmemory.h"
 
 using namespace std;
 int main()
 {
 	MockSerialStream *stream;
 	SystemFunctions *system;
-	//ModbusSerial<MockSerialStream, SystemFunctions, ModbusMemory> slave;
-	//slave.config(stream, system, 9600, -1);
+	ModbusSerial<MockSerialStream, SystemFunctions, ModbusMemory> slave;
+	slave.config(stream, system, 9600, -1);
 }
