30baa9cc5cdd43a45beadeb468ddcb06440bbd93
Alexander Kvenvolden <kvenclassical@gmail.com>
2018-07-24 08:11:32 -0700

    Added empty definition for ModbusMaster

---
+++ kwh-modbus/libraries/modbusMaster/ModbusMaster.hpp
@@ -0,0 +1,8 @@
+#include "../modbus/ModbusSerial.hpp"
+
+template<typename TSerial, typename TSystemFunctions, typename TBase>
+class ModbusMaster : public ModbusSerial<TSerial, TSystemFunctions, TBase>
+{
+public:
+
+};
